0ba1d281a23f7a11f9c4d415524cf62aee5bcf49
droal <droaltech@gmail.com>
2021-02-23 17:46:31 -0500

    Se aplicanestilos de material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatCardModule
   ],
-  exports:[
-    MatButtonModule
+  exports: [
+    MatButtonModule,
+    MatToolbarModule,
+    MatBadgeModule,
+    MatIconModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
